682b04e40684cb2e81e86562d32c5b325ca9d19b
Mesqualito <jochen@gebsattel.rocks>
2019-05-15 08:16:28 +0200

    https://oliverguenther.de/2014/04/applications-with-mojolicious-part-three-forms-and-login/ => Sessions

--- foto_db/lib/FotoDB.pm
+++ foto_db/lib/FotoDB.pm
@@ -11,6 +11,9 @@ sub startup {
   # Configure the application
   $self->secrets($config->{secrets});
 
+  # Default layout
+  $self->defaults(layout => 'base');
+
   # Router
     my $r = $self->routes;
 
@@ -19,8 +22,12 @@ sub startup {
   # $r->get('/')->to(template => 'main/index');
 
   # we can also change to route to use a controller to manually render data:
-  # $r->get('/')->to('main#index'); # short
-  $r->get('/')->to(controller => 'Main', action => 'index');
+  # $r->get('/')->to(controller => 'Main', action => 'index');
+  $r->get('/')->to('main#index'); # short
+
+  $r->get('/login')->name('login_form')->to(template => 'login/login_form');
+  $r->post('/login')->name('do_login')->to('Login#on_user_login');
+
 }
 
 1;
