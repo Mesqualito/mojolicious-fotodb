b06735b0cd5a29ab20315c5fe02405fb5bf33e66
Jochen Gebsattel <it@generica.net>
2019-07-11 11:14:12 +0200

    Each Mojolicious route has its own name which can be used to generate urls. Use 'url_for' or related functionality to generate a url by name, even placeholder values can be passed. 'link_to' generates link tags using route names. The first argument is the text that will be displayed by the link. The following arguments build the url by name and placeholder value(s). The return is actually a Mojo::URL so if adding other things like query parameters are needed, that can be done afterwards.

--- hello.pl
+++ hello.pl
@@ -1,13 +1,18 @@
 use Mojolicious::Lite;
 
-get '/:name' => {name => '🌍 world!'} => sub {
-	my $c = shift;
-	$c->render('hello');
-};
+get '/:name' => {template => 'staff'} => 'staff';
+get '/' => {template => 'home'} => 'home';
 
 app->start;
 
 __DATA__
 
-@@ hello.html.ep
-Hello <%= $name %>
+@@ staff.html.ep
+<p>This is <%= ucfirst $name %>.</p>
+
+@@ home.html.ep
+<p>Welcome to The North Pole!</p>
+
+<p>
+ Say hi to <%= link_to 'Santa' => staff => {name => 'santa'} %> and <%= link_to 'Rudolph' => staff => {name => 'rudolph'} %>.
+</p>
