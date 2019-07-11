14395be51fb4b8cc0d4a9d5644b010c1092deb02
Jochen Gebsattel <it@generica.net>
2019-07-11 11:43:09 +0200

    Changing the routes: keep the url's consistent, move all of the staff pages. Because the staff page urls were generated by name, we didn't need to change anything but the route definition.

--- hello.pl
+++ hello.pl
@@ -1,11 +1,15 @@
 use Mojolicious::Lite;
 
-get '/:name' => {template => 'staff'} => 'staff';
+get '/toy/:toy_name' => {template => 'toy'} => 'toy';
+get '/meet/:name' => {template => 'staff'} => 'staff';
 get '/' => {template => 'home'} => 'home';
 
 app->start;
 
 __DATA__
+
+@@ toy.html.ep
+<p>Look at this amazing <%= $toy_name %>.</p>
 
 @@ staff.html.ep
 <p>This is <%= ucfirst $name %>.</p>
@@ -16,3 +20,5 @@ __DATA__
 <p>
  Say hi to <%= link_to 'Santa' => staff => {name => 'santa'} %> and <%= link_to 'Rudolph' => staff => {name => 'rudolph'} %>.
 </p>
+<p>
+ And just wait until you see our amazing <%= link_to 'new puzzle' => toy => {toy_name => 'puzzle'} %>!</p>
